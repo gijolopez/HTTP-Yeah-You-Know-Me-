514d064b31d3e3580873ba57b897191192d31eba
gijolopez <glopez22@me.com>
2018-02-11 16:47:44 -0700

    added server class and first test to server_test file.

---
+++ test/server_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/server'
+
+class ServerTest < Minitest::Test
+
+  def test_it_exist
+    server = Server.new
+
+    assert_instance_of Server, server
+  end
+
+end
